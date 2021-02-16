a37ea7bb9c3fdd7d4bc35db3b22a4ae4e8350996
maxgdev <max@maxgdev.net>
2021-02-16 12:32:11 +0000

    Added intl package to format date field

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import './transaction.dart';
 
 void main() {
@@ -66,7 +67,7 @@ class MyHomePage extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                 Text(transaction.title, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
-                Text("${transaction.date}", style: TextStyle(color: Colors.grey),),
+                Text(DateFormat.yMMMd().format(transaction.date), style: TextStyle(color: Colors.grey),),
               ],)
             ],
           )
