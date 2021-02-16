e1865289b25cabebe260e1219ab2f8d6927fce2f
maxgdev <max@maxgdev.net>
2021-02-16 12:04:46 +0000

    Add code to parse transactions List and render to display

--- lib/main.dart
+++ lib/main.dart
@@ -27,31 +27,17 @@ class MyHomePage extends StatelessWidget {
   // List of Transactions
   final List<Transaction> transactions = [
     Transaction(
-      id: 't01',
-      title: 'Office Shirt',
-      amount: 19.99,
-      date: DateTime.now()
-    ),
+        id: 't01', title: 'Office Shirt', amount: 19.99, date: DateTime.now()),
     Transaction(
-      id: 't02',
-      title: 'Trainers',
-      amount: 49.99,
-      date: DateTime.now()
-    ),
+        id: 't02', title: 'Trainers', amount: 49.99, date: DateTime.now()),
     Transaction(
-      id: 't03',
-      title: 'Coffee mug',
-      amount: 5.99,
-      date: DateTime.now()
-    ),
+        id: 't03', title: 'Coffee mug', amount: 5.99, date: DateTime.now()),
     Transaction(
-      id: 't04',
-      title: 'Supermarket shopping',
-      amount: 27.54,
-      date: DateTime.now()
-    ),        
+        id: 't04',
+        title: 'Supermarket shopping',
+        amount: 27.54,
+        date: DateTime.now()),
   ];
-  
 
   @override
   Widget build(BuildContext context) {
@@ -60,62 +46,24 @@ class MyHomePage extends StatelessWidget {
         title: Text('Expenses App'),
       ),
       body: Column(
-        children: <Widget>[
-          Container(
-            width: double.infinity,
-            height: 100,
-            child: Card(
-              child: Text("This is where the chart will go"),
-              color: Colors.blue,
-              elevation: 5,
-            ),
+          // iterate over transactions variable
+          children: transactions.map((transaction) {
+        return Card(
+          child: Row(
+            children: [
+              Text("${transaction.amount}"),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                Text(transaction.title),
+                Text("${transaction.date}"),
+              ],)
+            ],
+          )
+        );
+      }).toList() // Convert transactions to iterable List
+
           ),
-          Container(
-            // Card #1 Container
-            width: double.infinity,
-            height: 40,
-            //  color: Colors.blue[200],
-            child: Card(
-              child: Text("Expenses item goes"),
-              color: Colors.blue[200],
-              elevation: 5,
-            ),
-          ),
-          Container(
-            // Card #2 Container
-            width: double.infinity,
-            height: 40,
-            //  color: Colors.blue[200],
-            child: Card(
-              child: Text("Expenses item goes"),
-              color: Colors.blue[200],
-              elevation: 5,
-            ),
-          ),
-          Container(
-            // Card #3 Container
-            width: double.infinity,
-            height: 40,
-            //  color: Colors.blue[200],
-            child: Card(
-              child: Text("Expenses item goes"),
-              color: Colors.blue[200],
-              elevation: 5,
-            ),
-          ),
-          Container(
-            // Card #4 Container
-            width: double.infinity,
-            height: 40,
-            //  color: Colors.blue[200],
-            child: Card(
-              child: Text("Expenses item goes"),
-              color: Colors.blue[200],
-              elevation: 5,
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
