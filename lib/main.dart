a7e3acb658f658a3a7f3bb508d166d70440b66f7
maxgdev <max@maxgdev.net>
2021-02-16 11:43:39 +0000

    Added dummy Transaction data

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main() {
   runApp(MyApp());
@@ -22,6 +23,35 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  // Define some dummy data
+  // List of Transactions
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't01',
+      title: 'Office Shirt',
+      amount: 19.99,
+      date: DateTime.now()
+    ),
+    Transaction(
+      id: 't02',
+      title: 'Trainers',
+      amount: 49.99,
+      date: DateTime.now()
+    ),
+    Transaction(
+      id: 't03',
+      title: 'Coffee mug',
+      amount: 5.99,
+      date: DateTime.now()
+    ),
+    Transaction(
+      id: 't04',
+      title: 'Supermarket shopping',
+      amount: 27.54,
+      date: DateTime.now()
+    ),        
+  ];
+  
 
   @override
   Widget build(BuildContext context) {
@@ -34,56 +64,56 @@ class MyHomePage extends StatelessWidget {
           Container(
             width: double.infinity,
             height: 100,
-            child: 
-            Card(
-              child:Text("This is where the chart will go"),
+            child: Card(
+              child: Text("This is where the chart will go"),
               color: Colors.blue,
               elevation: 5,
-            ),),
+            ),
+          ),
           Container(
-            // Card #1 Container 
-             width: double.infinity,
-             height: 40,
+            // Card #1 Container
+            width: double.infinity,
+            height: 40,
             //  color: Colors.blue[200],
-            child: 
-            Card(
-              child:Text("Expenses item goes"),
+            child: Card(
+              child: Text("Expenses item goes"),
               color: Colors.blue[200],
               elevation: 5,
-            ),),
+            ),
+          ),
           Container(
-            // Card #2 Container 
-             width: double.infinity,
-             height: 40,
+            // Card #2 Container
+            width: double.infinity,
+            height: 40,
             //  color: Colors.blue[200],
-            child: 
-            Card(
-              child:Text("Expenses item goes"),
+            child: Card(
+              child: Text("Expenses item goes"),
               color: Colors.blue[200],
               elevation: 5,
-            ),),
+            ),
+          ),
           Container(
-            // Card #3 Container 
-             width: double.infinity,
-             height: 40,
+            // Card #3 Container
+            width: double.infinity,
+            height: 40,
             //  color: Colors.blue[200],
-            child: 
-            Card(
-              child:Text("Expenses item goes"),
+            child: Card(
+              child: Text("Expenses item goes"),
               color: Colors.blue[200],
               elevation: 5,
-            ),),
+            ),
+          ),
           Container(
-            // Card #4 Container 
-             width: double.infinity,
-             height: 40,
+            // Card #4 Container
+            width: double.infinity,
+            height: 40,
             //  color: Colors.blue[200],
-            child: 
-            Card(
-              child:Text("Expenses item goes"),
+            child: Card(
+              child: Text("Expenses item goes"),
               color: Colors.blue[200],
               elevation: 5,
-            ),),                                        
+            ),
+          ),
         ],
       ),
     );
